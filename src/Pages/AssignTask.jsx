d32b71d9f0c6aa8a6ed16a19a45546ad8ab9dc6a
OmurRifat <jabedomur.ece.ku@gmail.com>
2023-06-13 23:20:47 +0600

    react-hook form added

--- src/Pages/AssignTask.jsx
+++ src/Pages/AssignTask.jsx
@@ -1,10 +1,24 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AssignTask = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
     return (
         <div>
-            <h4>This is assign task page.</h4>
+            <h4>Assign A Task.</h4>
+            <form onSubmit={ handleSubmit((data) => console.log(data)) }>
+                <input { ...register('firstName') } />
+                <input { ...register('lastName', { required: true }) } />
+                { errors.lastName && <p>Last name is required.</p> }
+                <input { ...register('age', { pattern: /\d+/ }) } />
+                { errors.age && <p>Please enter number for age.</p> }
+                <input type="submit" />
+            </form>
         </div>
     );
 };
