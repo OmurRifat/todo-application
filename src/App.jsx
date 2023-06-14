8fa93fc3d297e3fd9bcb040116a7d6c66fef87cf
OmurRifat <jabedomur.ece.ku@gmail.com>
2023-06-14 21:15:26 +0600

    loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Router';
+import { useEffect, useState } from 'react';
+import PuffLoader from "react-spinners/PuffLoader";
 
 function App() {
-
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    // Simulate a delay for demonstration purposes
+    setTimeout(() => setLoading(false), 3000);
+  }, []);
   return (
-    <div className="">
-      <RouterProvider router={ router }></RouterProvider>
+    <div>
+      {
+        loading ? <PuffLoader
+          className=' top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'
+          style={ { position: "absolute" } }
+          color="hsla(168, 67%, 53%, 1)"
+          size={ 100 }
+        /> : (
+          <RouterProvider router={ router }></RouterProvider>
+        )
+      }
     </div>
   )
 }
