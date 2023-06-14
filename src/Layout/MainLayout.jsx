9033906707fe2fb6be3294e7814ccc9c0526bd03
OmurRifat <jabedomur.ece.ku@gmail.com>
2023-06-14 15:55:00 +0600

    menubar issue solved

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -19,7 +19,18 @@ const MainLayout = () => {
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content w-full flex flex-col items-center my-4 mx-8">
                 {/* Page content here */ }
-                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                <label className="btn btn-circle swap swap-rotate lg:hidden absolute -top-4 left-1" htmlFor="my-drawer-2">
+
+                    {/* this hidden checkbox controls the state */ }
+                    <input type="checkbox" />
+
+                    {/* hamburger icon */ }
+                    <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" /></svg>
+
+                    {/* close icon */ }
+                    <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" /></svg>
+
+                </label>
                 <Outlet></Outlet>
             </div>
             <div className="drawer-side">
