5002c7049ec9a44752f46820c1836958270648db
OmurRifat <jabedomur.ece.ku@gmail.com>
2023-06-14 14:35:54 +0600

    some issue solved

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -20,9 +20,9 @@ const MainLayout = () => {
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80 h-full bg-secondary text-base-content text-center">
                     {/* Sidebar content here */ }
-                    <Link to="/tasks" className={ activeLink === "tasks" ? "active bg-slate-400 rounded-md drop-shadow shadow-inner text-base font-bold mb-2 py-3 text-blue-800" : 'py-3 bg-tertiary rounded-md mb-2' } onClick={ () => handleLinkClick('tasks') }>Tasks</Link>
-                    <Link to="/assign-task" className={ activeLink === "assign-task" ? "active bg-slate-400 rounded-md drop-shadow shadow-inner text-base font-bold mb-2 py-3 text-blue-800" : 'py-3 bg-tertiary rounded-md mb-2' } onClick={ () => handleLinkClick('assign-task') }>Assign Task</Link>
-                    <Link to="/all-employee" className={ activeLink === "all-employee" ? "active bg-slate-400 rounded-md drop-shadow shadow-inner text-base font-bold mb-2 py-3 text-blue-800" : 'py-3 bg-tertiary rounded-md mb-2' } onClick={ () => handleLinkClick('all-employee') }>All Employee</Link>
+                    <Link to="/tasks" className={ activeLink === "tasks" ? "active bg-slate-400 rounded-md drop-shadow shadow-inner text-base font-bold mb-2 py-3 text-blue-800" : 'py-3 bg-tertiary rounded-md mb-2 hover:bg-slate-700 hover:text-white' } onClick={ () => handleLinkClick('tasks') }>Tasks</Link>
+                    <Link to="/assign-task" className={ activeLink === "assign-task" ? "active bg-slate-400 rounded-md drop-shadow shadow-inner text-base font-bold mb-2 py-3 text-blue-800" : 'py-3 bg-tertiary rounded-md mb-2 hover:bg-slate-700 hover:text-white' } onClick={ () => handleLinkClick('assign-task') }>Assign Task</Link>
+                    <Link to="/all-employee" className={ activeLink === "all-employee" ? "active bg-slate-400 rounded-md drop-shadow shadow-inner text-base font-bold mb-2 py-3 text-blue-800" : 'py-3 bg-tertiary rounded-md mb-2 hover:bg-slate-700 hover:text-white' } onClick={ () => handleLinkClick('all-employee') }>All Employee</Link>
                 </ul>
             </div>
         </div>
