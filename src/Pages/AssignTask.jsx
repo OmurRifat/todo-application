74b32959f36ecb7f0e88f4d8f42773f77d5eb87e
OmurRifat <jabedomur.ece.ku@gmail.com>
2023-06-14 09:24:09 +0600

    task added to the localstorage

--- src/Pages/AssignTask.jsx
+++ src/Pages/AssignTask.jsx
@@ -4,20 +4,34 @@ import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../context/AuthProvider';
 import { useForm } from 'react-hook-form';
 import img from '../assets/notepad.png'
+import { toast } from 'react-hot-toast';
 
 const AssignTask = () => {
     const { allEmployee } = useContext(AuthContext);
     const {
         register,
         handleSubmit,
+        reset
     } = useForm();
     const onSubmit = (data) => {
-        console.log(data);
+        let task = localStorage.getItem('task');
+        let updatedData = [];
+        if (task === null) {
+            localStorage.setItem('task', JSON.stringify([data]));
+            toast.success("Task Assigned Successfully");
+            reset();
+        }
+        else {
+            let previousData = JSON.parse(task);
+            updatedData = [...previousData, data];
+            localStorage.setItem('task', JSON.stringify(updatedData));
+            toast.success("Task Assigned Successfully");
+            reset();
+        }
     };
 
     return (
         <div className=''>
-            <h4>Assign A Task.</h4>
             <div className=' grid grid-cols-2 items-center m-20'>
                 <div>
                     <img className='' src={ `${img}` } alt="" />
@@ -26,6 +40,7 @@ const AssignTask = () => {
                     <form onSubmit={ handleSubmit(onSubmit) } className=''>
                         <legend className=' mb-2 mt-4'>Select an employee</legend>
                         <select required { ...register("name") } className="select w-full max-w-sm mb-4">
+                            <option defaultValue={ "Please select an employee" } disabled >Please select an employee</option>
                             {
                                 allEmployee?.map(employee => <option key={ employee.id } value={ employee.name }>{ employee.email }-{ employee.name }</option>)
                             }
@@ -33,7 +48,10 @@ const AssignTask = () => {
                         <legend className=' mb-2'>Task ID</legend>
                         <input required { ...register("id") } type="text" placeholder="Give the ID of your task" className="input w-full max-w-sm mb-4" />
                         <legend className=' mb-2'>Task Name</legend>
-                        <input required { ...register("taskName") } type="text" placeholder="Give a title of your task" className="input w-full max-w-sm mb-4" /> <br />
+                        <input required { ...register("taskName") } type="text" placeholder="Give a title of your task" className="input w-full max-w-sm mb-4" />
+                        <legend className=' mb-2'>Task Description</legend>
+                        <textarea required { ...register("taskDescription") } className="textarea w-full max-w-sm mb-4" placeholder="Task Description"></textarea>
+                        <br />
                         <button className=' btn btn-primary mb-10' type='submit'>Assign</button>
                     </form>
                 </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import AuthProvider from './context/AuthProvider'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
+      <Toaster />
     </AuthProvider>
   </React.StrictMode>,
 )
